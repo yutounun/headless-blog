5b3a32eba5d8cd60ea9f5549ef6a62b667468de2
yutounun <geek.yuto@gmail.com>
2024-09-21 21:00:42 -0700

    first commit including sass settings

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import "@/app/styles/styles.sass";
+import { Lato, Open_Sans } from "next/font/google";
+
+// heading font
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700", "900"],
+  variable: "--font-lato",
+});
+
+// body font
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-open-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Andrew's Blog",
+  description: "Andrew's Blog",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={` ${openSans.className} ${openSans.variable} ${lato.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
